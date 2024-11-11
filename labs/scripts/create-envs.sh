dcea5a0c9198b1a9e825f41480fa762fb52df242
William Munoz <william@godeveloper.io>
2024-11-11 16:34:33 -0500

    install metrics server on lab07

--- labs/scripts/create-envs.sh
+++ labs/scripts/create-envs.sh
@@ -15,6 +15,11 @@ kubectl apply -f yaml-definitions/statefulset-o3db.yaml
 # Lab 04
 kubectl apply -f yaml-definitions/lab4-service-am-i-ready.yaml
 
+# Lab 07
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update
+helm upgrade --install --set args={--kubelet-insecure-tls} metrics-server metrics-server/metrics-server --namespace kube-system
+
 # Lab 11, 12, 17
 kubectl create namespace project-tiger
 
